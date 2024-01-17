dc87c6500ce71846464deba2fed7cf7719372f53
Eunsong Koh <eunsongkohh@gmail.com>
2024-01-17 13:02:48 -0800

    Added POlaris and removed services file with new counting implementation

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,18 +1,29 @@
 import React from "react";
+import StopWatchButton from "./StopWatchButton";
+import { useState, useEffect } from "react";
+import { Page, Text } from "@shopify/polaris";
 
 /*
 TODO: 
 - should start counting when the user clicks the start button 
 - should stop counting when the use clicks the stop button 
 */
-import StopWatchButton from "./StopWatchButton";
 export default function StopWatch() {
+  const [time, setTime] = useState(0);
+
+  const updateTime = (currentTime: number) => {
+    setTime(currentTime);
+    console.log(time);
+  };
+
   return (
-    <div>
-      <span>TIME</span>
+    <Page>
+      <Text variant="heading3xl" as="h2">
+        {time}
+      </Text>
       <br />
       <span>Laps Logged</span>
-      <StopWatchButton />
-    </div>
+      <StopWatchButton displayTime={updateTime} />
+    </Page>
   );
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
+import { Button, ButtonGroup } from "@shopify/polaris";
 
-export default function StopWatchButton() {
-    function editTime(){
+interface StopWatchButtonProps {
+  displayTime: (currentTime: number) => void;
+}
+export default function StopWatchButton({ displayTime }: StopWatchButtonProps) {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [currentLapCount, setLapCount] = useState(false);
+  const [startStop, setStartStop] = useState("Start");
+  var interval: any;
 
+  const editTime = () => {
+    if (startStop === "Start") {
+      setStartStop("Stop");
+      interval = setInterval(() => {
+        setCurrentTime((prevcurrentTime) => prevcurrentTime + 1);
+        displayTime(currentTime);
+      }, 1000);
+    } else {
+      console.log("here");
+      setStartStop("Stop");
+      clearInterval(interval);
     }
+  };
 
-    function resetTime(){
-
-    }
+  const resetTime = () => {};
+  const addLap = () => {};
 
   return (
-    <div>
+    <ButtonGroup variant="segmented">
       {/* Buttons for the stopwatch   */}
-      <button onClick={editTime}>Start</button>
-      <button>Reset</button>
-      <button onClick={resetTime}>Lap</button>
-    </div>
+      <Button onClick={editTime}>{startStop}</Button>
+      <Button onClick={resetTime}>Reset</Button>
+      <Button onClick={addLap}>Lap</Button>
+    </ButtonGroup>
   );
 }
