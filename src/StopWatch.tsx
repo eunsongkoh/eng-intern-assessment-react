1d5954c11bd3a5b19e93fb33d8af9e5c7889eae2
Eunsong Koh <eunsongkohh@gmail.com>
2024-01-17 18:36:35 -0800

    Finished button and stopwatch function ality

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -3,27 +3,31 @@ import StopWatchButton from "./StopWatchButton";
 import { useState, useEffect } from "react";
 import { Page, Text } from "@shopify/polaris";
 
-/*
-TODO: 
-- should start counting when the user clicks the start button 
-- should stop counting when the use clicks the stop button 
-*/
 export default function StopWatch() {
   const [time, setTime] = useState(0);
+  const [laps, setLaps] = useState(0);
+  const [activateLaps, setActivateLaps] = useState(false);
 
+  // updates the time of the stopwatch
   const updateTime = (currentTime: number) => {
     setTime(currentTime);
-    console.log(time);
+  };
+
+  // updates the laps of the stopwatch
+  const updateLaps = (currentLapCount: number) => {
+    setLaps(currentLapCount);
+    setActivateLaps(true);
   };
 
   return (
     <Page>
       <Text variant="heading3xl" as="h2">
-        {time}
+        {time} (in seconds)
       </Text>
       <br />
-      <span>Laps Logged</span>
-      <StopWatchButton displayTime={updateTime} />
+      {/* displays the amount of laps once lap is clicked */}
+      {activateLaps && <h2>Laps: {laps}</h2>}
+      <StopWatchButton displayTime={updateTime} displayLaps={updateLaps} />
     </Page>
   );
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,36 +1,65 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { Button, ButtonGroup } from "@shopify/polaris";
 
+// props to pass into the stop watch button
+// allows for communicate with the StopWatch and StopWatchButton files
 interface StopWatchButtonProps {
   displayTime: (currentTime: number) => void;
+  displayLaps: (currentLaps: number) => void;
 }
-export default function StopWatchButton({ displayTime }: StopWatchButtonProps) {
+export default function StopWatchButton({
+  displayTime,
+  displayLaps,
+}: StopWatchButtonProps) {
+  // states for laps, timer buttons, and time on the stopwatch
   const [currentTime, setCurrentTime] = useState(0);
-  const [currentLapCount, setLapCount] = useState(false);
-  const [startStop, setStartStop] = useState("Start");
-  var interval: any;
+  const [currentLapCount, setLapCount] = useState(1);
 
-  const editTime = () => {
-    if (startStop === "Start") {
-      setStartStop("Stop");
-      interval = setInterval(() => {
-        setCurrentTime((prevcurrentTime) => prevcurrentTime + 1);
-        displayTime(currentTime);
-      }, 1000);
+  const [startStop, setStartStop] = useState(false);
+  const [runButton, setrunButton] = useState("Start");
+
+  var intervalID: any;
+
+  // modifies the start/stop button and stops/starts the stopwatch
+  const stopwatchRunning = () => {
+    setStartStop(!startStop);
+    if (startStop === false) {
+      setrunButton("Stop");
     } else {
-      console.log("here");
-      setStartStop("Stop");
-      clearInterval(interval);
+      setrunButton("Start");
     }
   };
 
-  const resetTime = () => {};
-  const addLap = () => {};
+  // starts the stopwatch
+  useEffect(() => {
+    if (startStop) {
+      intervalID = setInterval(() => setCurrentTime(currentTime + 1), 1000);
+      displayTime(currentTime);
+    }
+
+    // pauses the stopwatch
+    return () => clearInterval(intervalID);
+  }, [startStop, currentTime]);
+
+  // resets the stopwatch
+  const resetTime = () => {
+    console.log("resetting");
+    setCurrentTime(0);
+    displayTime(0);
+    clearInterval(intervalID);
+  };
+
+  // adds to the total lap count
+  const addLap = () => {
+    setLapCount(currentLapCount + 1);
+    displayLaps(currentLapCount);
+  };
 
   return (
     <ButtonGroup variant="segmented">
-      {/* Buttons for the stopwatch   */}
-      <Button onClick={editTime}>{startStop}</Button>
+      {/* Buttons for the stopwatch*/}
+      <Button onClick={stopwatchRunning}>{runButton}</Button>
       <Button onClick={resetTime}>Reset</Button>
       <Button onClick={addLap}>Lap</Button>
     </ButtonGroup>
